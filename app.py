391a8c587da62ad76455086bbfc684a80f3a3758
riziqfajri10 <riziqf155@gmail.com>
2026-07-15 10:53:41 +0700

    Update app.py untuk deploy Render

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -53,5 +53,11 @@ def contact():
         }
     })
 
+# if __name__ == "__main__":
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+    
+    # app.run(debug=True)
